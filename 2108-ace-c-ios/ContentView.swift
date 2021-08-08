8e1c24567ba412e8821c72a6009dc4eddc8efaf4
KOTARO SUTO <ng1a5ss43@gmail.com>
2021-08-08 17:06:43 +0900

    Remove unnecessary comments

--- 2108-ace-c-ios/ContentView.swift
+++ 2108-ace-c-ios/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  2108-ace-c-ios
-//
-//  Created by Kotaro Suto on 2021/08/08.
-//
-
 import SwiftUI
 
 struct ContentView: View {
--- 2108-ace-c-ios/_108_ace_c_iosApp.swift
+++ 2108-ace-c-ios/_108_ace_c_iosApp.swift
@@ -1,10 +1,3 @@
-//
-//  _108_ace_c_iosApp.swift
-//  2108-ace-c-ios
-//
-//  Created by Kotaro Suto on 2021/08/08.
-//
-
 import SwiftUI
 
 @main
