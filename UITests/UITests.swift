d5363301514fc000f43eae6178aa6594b67d408c
KOTARO SUTO <ng1a5ss43@gmail.com>
2021-08-10 17:39:14 +0900

    Rename auto-generated class, struct, and file.

--- UITests/UITests.swift
+++ UITests/UITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class _108_ace_c_iosUITests: XCTestCase {
+class AceCiOSUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
