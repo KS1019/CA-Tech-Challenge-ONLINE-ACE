936ac6134364e5c3c55d56e990518241962b7b85
techiro <techiromen0@gmail.com>
2021-08-11 23:51:44 +0900

    refactor: 使っていないtestファイルのコードを削除

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -4,30 +4,9 @@
 //
 //  Created by Kotaro Suto on 2021/08/08.
 //
-
+@testable import ace_c_ios
 import XCTest
-@testable import _108_ace_c_ios
 
 class AceCiOSTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
