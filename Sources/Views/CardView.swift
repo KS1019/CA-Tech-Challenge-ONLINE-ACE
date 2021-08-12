409db19a9bc7e851d54e129bb801ad8506f0c391
KOTARO SUTO <ng1a5ss43@gmail.com>
2021-08-12 15:19:14 +0900

    add: cardView

--- Sources/Views/CardView.swift
+++ Sources/Views/CardView.swift
@@ -8,18 +8,19 @@
 import SwiftUI
 
 struct CardView: View {
+    var timeTable: TimeTable
     var body: some View {
         HStack {
             timeText
             programInfo
         }
-        
     }
     
     private var programInfo: some View {
         ZStack {
             Color.yellow
             
+            // サムネイルがあればここに追加
             Image("")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
@@ -28,21 +29,26 @@ struct CardView: View {
     }
     
     private var timeText: some View {
-        ZStack {
-            Color.white
-            VStack {
-                Spacer()
-                Text("8:00")
-                Spacer()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        return
+            ZStack {
+                Color.white
+                VStack {
+                    Spacer()
+                    Text(formatter.string(from: Date(timeIntervalSince1970: TimeInterval(timeTable.startAt))))
+                    Text("~")
+                    Text(formatter.string(from: Date(timeIntervalSince1970: TimeInterval(timeTable.endAt))))
+                    Spacer()
+                }
             }
-        }
-        .frame(width: 30)
+            .frame(width: 60)
     }
 }
 
 struct CardView_Previews: PreviewProvider {
     static var previews: some View {
-        CardView()
+        CardView(timeTable: TimeTable(id: "1234", title: "Test Title", startAt: 1627232880, endAt: 1627237860, channelId: "9876", labels: ["" : false]))
             .previewLayout(.sizeThatFits)
     }
 }
