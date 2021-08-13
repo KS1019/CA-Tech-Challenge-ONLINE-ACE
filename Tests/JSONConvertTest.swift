f840af7d3711e676282a728f9ad8ae4d1f69cff3
KOTARO SUTO <ng1a5ss43@gmail.com>
2021-08-14 01:31:35 +0900

    fix: swiftlint warnings

--- Tests/JSONConvertTest.swift
+++ Tests/JSONConvertTest.swift
@@ -10,7 +10,25 @@ import XCTest
 
 class JSONConvertTest: XCTestCase {
     func test_JSONConversion() throws {
-        let json = """
+        let valueFromJSON = try XCTUnwrap(JSONDecoder().decode(TimeTable.self, from: JSONConvertTest.json))
+
+        let expectedValue = TimeTable(id: "EQYyywjosSkxUX", title: "ENLIGHT #1", highlight: "t", detailHighlight: "",
+                                      startAt: 1_627_232_880, endAt: 1_627_237_860, channelId: "fishing",
+                                      labels: ["live": false, "first": false, "last": false, "bundle": false, "new": false, "pickup": false],
+                                      displayProgram: DisplayProgram(credit:
+                                                                        DisplayProgram.Credit(casts: ["川村光太郎"],
+                                                                                              crews: ["プロデューサー:松尾健司"],
+                                                                                              copyrights: ["(C)テレビ朝日"]),
+                                                                     // swiftlint:disable line_length
+                                                                     content: "「いつも通り、目一杯釣るだけ! 簡単にはいかないと思いますが、苦しむ僕を見てください!」と話す 日本最高レベルの岸釣りアングラー川村光大郎の新番組『ENLIGHT』初回は霞ケ浦の流入河川で40UPを狙う!"))
+
+        XCTAssertEqual(valueFromJSON, expectedValue)
+    }
+}
+
+extension JSONConvertTest {
+    // swiftlint:disable force_unwrapping
+    static let json = """
         {"id": "EQYyywjosSkxUX",
             "title": "ENLIGHT #1",
             "startAt": 1627232880,
@@ -48,11 +66,4 @@ class JSONConvertTest: XCTestCase {
             }
         }
         """.data(using: .utf8)!
-
-        let valueFromJSON = try XCTUnwrap(JSONDecoder().decode(TimeTable.self, from: json))
-        
-        let expectedValue = TimeTable(id: "EQYyywjosSkxUX", title: "ENLIGHT #1", highlight: "t", detailHighlight: "", startAt: 1627232880, endAt: 1627237860, channelId: "fishing", labels: ["live": false, "first": false, "last": false, "bundle": false, "new": false, "pickup": false], displayProgram: DisplayProgram(credit: DisplayProgram.Credit(casts: ["川村光太郎"], crews: ["プロデューサー:松尾健司"], copyrights: ["(C)テレビ朝日"]), content: "「いつも通り、目一杯釣るだけ! 簡単にはいかないと思いますが、苦しむ僕を見てください!」と話す 日本最高レベルの岸釣りアングラー川村光大郎の新番組『ENLIGHT』初回は霞ケ浦の流入河川で40UPを狙う!"))
-        
-        XCTAssertEqual(valueFromJSON, expectedValue)
-    }
 }
