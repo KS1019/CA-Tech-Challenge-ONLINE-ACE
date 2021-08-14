8c68ad09e1ff859e0fa929240c14b360c3a33d6b
KOTARO SUTO <ng1a5ss43@gmail.com>
2021-08-14 18:44:52 +0900

    cleanup: Dangerfile.swift

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -4,10 +4,10 @@ let danger = Danger()
 let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
 
 if allSourceFiles.first(where: { $0.fileType == .swift }) != nil {
-  let violation = SwiftLint.lint()
-  if violation.isEmpty {
-    message("No violation found!!!")
-  }
+    let violation = SwiftLint.lint()
+    if violation.isEmpty {
+        message("No violation found!!!")
+    }
 } else {
-  message("No .swift file was added")
+    message("No .swift file was added")
 }
