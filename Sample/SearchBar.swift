a179f977a097988841162b0b8a2c26e034f5b577
techiro <techiromen0@gmail.com>
2021-08-15 16:30:48 +0900

    add: SearchBarViewの追加

---
+++ Sample/SearchBar.swift
@@ -0,0 +1,63 @@
+//
+//  SearchBar.swift
+//  App
+//
+//  Created by TanakaHirokazu on 2021/08/15.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    @Binding var query: String
+    @Binding var isEditing: Bool
+
+    var search: () -> Void
+
+    var body: some View {
+        HStack {
+            HStack {
+                Image(systemName: "magnifyingglass")
+                TextField("キーワードを検索してください", text: $query,
+
+                          onEditingChanged: { begin in
+
+                            isEditing = begin ? true : false
+
+                          },
+                          // リターンキーが押された時の処理
+                          onCommit: {
+                            search()
+                          })
+                    .foregroundColor(.primary)
+                Button(action: {
+                    query = ""
+                }) {
+                    Image(systemName: "xmark.circle.fill").opacity(query == "" ? 0 : 1)
+                }
+            }
+            .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
+            .foregroundColor(.secondary)
+            .background(Color(.secondarySystemBackground))
+            .cornerRadius(10.0)
+
+            if isEditing {
+                Button("Cancel") {
+                    query = ""
+                    UIApplication.shared.closeKeyboard()
+                }
+            }
+
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct SearchBar_Previews: PreviewProvider {
+    static func getInfo() {
+        print("テスト")
+    }
+    static var previews: some View {
+
+        SearchBar(query: .constant(" "), isEditing: .constant(false), search: getInfo)
+    }
+}
