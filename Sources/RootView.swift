b52219cb80cdf1c9dc381d762ea6637dcdb7b906
techiro <techiromen0@gmail.com>
2021-08-16 15:14:33 +0900

    refactor: Tabs Model

--- Sources/RootView.swift
+++ Sources/RootView.swift
@@ -41,29 +41,3 @@ struct RootView_Previews: PreviewProvider {
         RootView()
     }
 }
-
-enum Tabs: CustomStringConvertible {
-
-    case calendar, channel, reserved
-
-    var description: String {
-        switch self {
-        case .calendar:
-            return "Calendar"
-        case .channel:
-            return "Channel"
-        case .reserved:
-            return "Reserved"
-        }
-    }
-    var systemimage: String {
-        switch self {
-        case .calendar:
-            return "calendar"
-        case .channel:
-            return "square.grid.2x2"
-        case .reserved:
-            return "alarm"
-        }
-    }
-}
