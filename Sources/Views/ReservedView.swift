b049f1bb52e2b99cd300b07a62131b0d8531deca
KOTARO SUTO <ng1a5ss43@gmail.com>
2021-08-20 22:30:57 +0900

    add: ReservedView

--- Sources/Views/ReservedView.swift
+++ Sources/Views/ReservedView.swift
@@ -7,14 +7,40 @@
 
 import SwiftUI
 
-struct ReservedView: View {
+struct ReservedView<T: TimeTableViewModelProtocol>: View {
+    @StateObject var vm: T
+    @State var aWeek: [Date]? = Date.getWeek()
+    @State private var selectedIndex: Int = 0
     var body: some View {
-        Text("ReservedView")
+        VStack {
+            HorizontalPickerView(selection: $selectedIndex, selections: aWeek ?? [Date()])
+
+            ScrollView {
+                LazyVStack {
+                    ForEach(vm.reservations.filter {
+                                aWeek![selectedIndex] <= Date(timeIntervalSince1970: TimeInterval($0.startAt)) ||
+                                Date(timeIntervalSince1970: TimeInterval($0.endAt)) <= aWeek![selectedIndex] })
+                    { timetable in
+                        VStack(alignment: .leading) {
+                            CardView(timeTable: timetable)
+                        }
+                    }
+                }
+                if vm.isLoading {
+                    activityIndicator
+                }
+            }
+            
+        }
+    }
+    private var activityIndicator: some View {
+        ActivityIndicator(style: .medium)
+            .frame(idealWidth: .infinity, maxWidth: .infinity, alignment: .center)
     }
 }
 
 struct ReservedView_Previews: PreviewProvider {
     static var previews: some View {
-        ReservedView()
+        ReservedView(vm: MockTimeTableViewModel())
     }
 }
