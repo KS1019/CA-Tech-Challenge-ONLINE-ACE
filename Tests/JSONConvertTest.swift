a53f6714db22ecb49507d48ceab0ec5dbf4832af
techiro <techiromen0@gmail.com>
2021-08-21 13:48:05 +0900

    fix: jsonのテストの修正

--- Tests/JSONConvertTest.swift
+++ Tests/JSONConvertTest.swift
@@ -12,9 +12,9 @@ class JSONConvertTest: XCTestCase {
     func test_JSONの変換() throws {
         let valueFromJSON = try XCTUnwrap(JSONDecoder().decode(TimeTable.self, from: JSONConvertTest.json))
 
-        let expectedValue = TimeTable(id: "EQYyywjosSkxUX", title: "ENLIGHT #1", highlight: "t", detailHighlight: "",
-                                      startAt: 1_627_232_880, endAt: 1_627_237_860, channelId: "fishing",
-                                      labels: ["live", "new"], content: "content")
+        let expectedValue = TimeTable(id: "EQYyywjosSkxUX", title: "黒子のバスケ 第1期 全話一挙", highlight: "黒子のバスケ 第1期 全話一挙", detailHighlight: "",
+                                      startAt: 1_626_238_800, endAt: 1_626_278_400, channelId: "abema-anime-2",
+                                      labels: ["bundle"], content: "")
 
         XCTAssertEqual(valueFromJSON, expectedValue)
     }
@@ -23,41 +23,14 @@ class JSONConvertTest: XCTestCase {
 extension JSONConvertTest {
     // swiftlint:disable force_unwrapping
     static let json = """
-        {"id": "EQYyywjosSkxUX",
-            "title": "ENLIGHT #1",
-            "startAt": 1627232880,
-            "endAt": 1627237860,
-            "channelId": "fishing",
-            "groupId": "",
-            "highlight": "t",
-            "detailHighlight": "",
-            "content": "",
-            "labels": {
-                "live": false,
-                "first": false,
-                "last": false,
-                "bundle": false,
-                "new": false,
-                "pickup": false
-            },
-            "displayProgram": {
-                "id": "151-95_s0_p1",
-                "series": {
-                    "id": "151-95"
-                },
-                "credit": {
-                    "casts": [
-                        "川村光太郎"
-                    ],
-                    "crews": [
-                        "プロデューサー:松尾健司"
-                    ],
-                    "copyrights": [
-                        "(C)テレビ朝日"
-                    ]
-                },
-                "content": "「いつも通り、目一杯釣るだけ! 簡単にはいかないと思いますが、苦しむ僕を見てください!」と話す 日本最高レベルの岸釣りアングラー川村光大郎の新番組『ENLIGHT』初回は霞ケ浦の流入河川で40UPを狙う!"
-            }
-        }
+        {"id":"EQYyywjosSkxUX",
+        "title":"黒子のバスケ 第1期 全話一挙",
+        "channel_id":"abema-anime-2",
+        "labels":["bundle"],
+        "highlight":"黒子のバスケ 第1期 全話一挙",
+        "detail_highlight":"",
+        "content":"",
+        "start_at":1626238800,
+        "end_at":1626278400}
         """.data(using: .utf8)!
 }
