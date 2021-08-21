da8e76e0c5a9195f2eda94b4f17b4a19d0690640
KOTARO SUTO <ng1a5ss43@gmail.com>
2021-08-21 11:55:33 +0900

    add: GenreFilterView to ChannelView

--- Sources/Views/ChannelView.swift
+++ Sources/Views/ChannelView.swift
@@ -10,14 +10,22 @@ import SwiftUI
 struct ChannelView<T: TimeTableViewModelProtocol>: View {
     @StateObject var vm: T
     @State private var selectedIndex: Int = 0
+    @State private var selectedGenreFilters: [String: Bool] = [:]
+    @State private var filteredTimetables: [T.ListData] = []
     var body: some View {
         VStack {
             // vmからチャンネル一覧を取得
             HorizontalPickerView(selection: $selectedIndex, selections: vm.channels.map { $0.title })
 
+            GenreFilterView(selectedGenres: $selectedGenreFilters)
+
             ScrollView {
                 LazyVStack {
-                    ForEach(vm.timetables.filter { $0.channelId == vm.channels[selectedIndex].id }) { timetable in
+                    ForEach(
+                        vm.timetables.filter { timetable in
+                            timetable.channelId == vm.channels[selectedIndex].id &&
+                                !timetable.labels.filter { label in selectedGenreFilters.filter { dic in dic.value }.keys.sorted().contains(label) }.isEmpty
+                        }) { timetable in
                         VStack(alignment: .leading) {
                             CardView(timeTable: timetable)
                         }
@@ -28,6 +36,14 @@ struct ChannelView<T: TimeTableViewModelProtocol>: View {
                 }
             }
 
+        }
+        .onAppear {
+            let labels: [String] = Array(Set(vm.timetables.filter { !$0.labels.isEmpty }.map { $0.labels }.joined()))
+            selectedGenreFilters = labels.reduce([String: Bool]()) { (result, label)  in
+                var newResult = result
+                newResult[label] = false
+                return newResult
+            }
         }
     }
     private var activityIndicator: some View {
