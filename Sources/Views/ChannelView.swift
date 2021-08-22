c6768b902b3cf1b9c5bc6751ceb855c316007d68
techiro <techiromen0@gmail.com>
2021-08-22 09:50:15 +0900

    fix: ChannelViewをChannelViewModelに依存させた

--- Sources/Views/ChannelView.swift
+++ Sources/Views/ChannelView.swift
@@ -4,28 +4,25 @@
 //
 //  Created by TanakaHirokazu on 2021/08/16.
 //
-
+import Combine
 import SwiftUI
 
-struct ChannelView<T: TimeTableViewModelProtocol>: View {
-    @StateObject var vm: T
-    @State private var selectedIndex: Int = 0
-    @State private var selectedGenreFilters: [String: Bool] = [:]
-    @State private var filteredTimetables: [T.ListData] = []
+struct ChannelView: View {
+    @StateObject var vm = ChannelViewModel(repository: TimeTableRepositoryImpl())
     var body: some View {
         VStack {
             // vmからチャンネル一覧を取得
-            HorizontalPickerView(selection: $selectedIndex, selections: vm.channels.map { $0.title })
+            HorizontalPickerView(selection: $vm.selectedIndex, selections: vm.channels.map { $0.title })
 
-            GenreFilterView(selectedGenres: $selectedGenreFilters)
+            GenreFilterView(selectedGenres: $vm.selectedGenreFilters)
 
             ScrollView {
                 LazyVStack {
                     ForEach(
                         vm.timetables.filter { timetable in
-                            timetable.channelId == vm.channels[selectedIndex].id &&
-                                (!timetable.labels.filter { label in selectedGenreFilters.filter { dic in dic.value }.keys.sorted().contains(label) }.isEmpty
-                                    || !selectedGenreFilters.values.contains(true))
+                            timetable.channelId == vm.channels[vm.selectedIndex].id &&
+                                (!timetable.labels.filter { label in vm.selectedGenreFilters.filter { dic in dic.value }.keys.sorted().contains(label) }.isEmpty
+                                    || !vm.selectedGenreFilters.values.contains(true))
                         }) { timetable in
                         VStack(alignment: .leading) {
                             CardView(timeTable: timetable)
@@ -38,19 +35,13 @@ struct ChannelView<T: TimeTableViewModelProtocol>: View {
             }
 
         }
-        .onAppear {
-            let labels: [String] = Array(Set(vm.timetables.filter { !$0.labels.isEmpty }.map { $0.labels }.joined()))
-            selectedGenreFilters = labels.reduce([String: Bool]()) { (result, label)  in
-                var newResult = result
-                newResult[label] = false
-                return newResult
-            }
-        }
+        .onAppear(perform: vm.onAppear)
     }
-    private var activityIndicator: some View {
-        ActivityIndicator(style: .medium)
-            .frame(idealWidth: .infinity, maxWidth: .infinity, alignment: .center)
-    }
+}
+private var activityIndicator: some View {
+    ActivityIndicator(style: .medium)
+        .frame(idealWidth: .infinity, maxWidth: .infinity, alignment: .center)
+}
 
 struct ChannelView_Previews: PreviewProvider {
     static var previews: some View {
