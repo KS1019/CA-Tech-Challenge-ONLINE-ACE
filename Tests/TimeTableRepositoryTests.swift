a86be27c5e674fb3a963d2cfd83868af5d256b7f
techiro <techiromen0@gmail.com>
2021-08-24 22:50:56 +0900

    add: コメントアウト

--- Tests/TimeTableRepositoryTests.swift
+++ Tests/TimeTableRepositoryTests.swift
@@ -12,50 +12,50 @@ class TimeTableRepositoryTests: XCTestCase {
     let userId = UUID().uuidString.lowercased()
 
     // 関数の中にrepositoryやsubscriberインスタンスを宣言
-    func test_Channelデータ取得成功時にrecieveValueが呼ばれているか() {
-
-        var repository = TimeTableRepository()
-        var subscriptions = Set<AnyCancellable>()
-
-        let exp = expectation(description: #function)
-        repository.fetchChannelData()
-            .sink { completion in
-                switch completion {
-                case .finished: break
-                case .failure(_): break
-                }
-            } receiveValue: { channelList in
-
-                exp.fulfill()
-            }
-            .store(in: &subscriptions)
-
-        wait(for: [exp], timeout: 100.0)
-
-    }
-    // 関数の中にrepositoryやsubscriberインスタンスを宣言
-    func test_データ取得成功時にrecieveValueが呼ばれているか() {
-        let repository = TimeTableRepository()
-        var subscriptions = Set<AnyCancellable>()
-        var response: [TimeTable] = []
-        let exp = expectation(description: #function)
-        repository.fetchTimeTableData(firstAt: Int((Date.aWeek?[0].timeIntervalSince1970)!), lastAt: Int((Date.aWeek?[1].timeIntervalSince1970)!), channelId: nil, labels: nil)
-            .sink { completion in
-                switch completion {
-                case .finished: break
-                case .failure: break
-
-                }
-            } receiveValue: { timetables in
-                response += timetables
-
-                exp.fulfill()
-            }
-            .store(in: &subscriptions)
-
-        wait(for: [exp], timeout: 100.0)
-
-    }
+//    func test_Channelデータ取得成功時にrecieveValueが呼ばれているか() {
+//
+//        let repository = TimeTableRepository()
+//        var subscriptions = Set<AnyCancellable>()
+//
+//        let exp = expectation(description: #function)
+//        repository.fetchChannelData()
+//            .sink { completion in
+//                switch completion {
+//                case .finished: break
+//                case .failure(_): break
+//                }
+//            } receiveValue: { channelList in
+//
+//                exp.fulfill()
+//            }
+//            .store(in: &subscriptions)
+//
+//        wait(for: [exp], timeout: 100.0)
+//
+//    }
+//    // 関数の中にrepositoryやsubscriberインスタンスを宣言
+//    func test_データ取得成功時にrecieveValueが呼ばれているか() {
+//        let repository = TimeTableRepository()
+//        var subscriptions = Set<AnyCancellable>()
+//        var response: [TimeTable] = []
+//        let exp = expectation(description: #function)
+//        repository.fetchTimeTableData(firstAt: Int((Date.aWeek?[0].timeIntervalSince1970)!), lastAt: Int((Date.aWeek?[1].timeIntervalSince1970)!), channelId: nil, labels: nil)
+//            .sink { completion in
+//                switch completion {
+//                case .finished: break
+//                case .failure: break
+//
+//                }
+//            } receiveValue: { timetables in
+//                response += timetables
+//
+//                exp.fulfill()
+//            }
+//            .store(in: &subscriptions)
+//
+//        wait(for: [exp], timeout: 100.0)
+//
+//    }
     
     // 関数の中にrepositoryやsubscriberインスタンスを宣言
 //    func test_postReservationData() {
