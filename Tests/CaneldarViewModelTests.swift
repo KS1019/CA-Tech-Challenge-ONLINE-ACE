36f59e350198c38d5248aa701d64dd5b5f62b209
techiro <techiromen0@gmail.com>
2021-08-27 15:39:52 +0900

    add: CarendarVMのonAppear時のテスト

--- Tests/CaneldarViewModelTests.swift
+++ Tests/CaneldarViewModelTests.swift
@@ -4,28 +4,40 @@
 //
 //  Created by TanakaHirokazu on 2021/08/27.
 //
-
+import Combine
 import XCTest
 
 class CaneldarViewModelTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
+
     }
 
     func test_updateRepositoriesWhenOnAppear() {
+        //        let vm = CalendarViewModel(repository: MockTimeTableRepository())
+        //        以下はvm.onAppear()の処理
         let repository = MockTimeTableRepository()
+        var subscriptions = Set<AnyCancellable>()
+
         let exp = expectation(description: #function)
-        repository.fetchTimeTableData(firstAt: 1, lastAt: 1, channelId: nil, labels: nil)
+        repository.fetchTimeTableData(firstAt: 1, lastAt: 2, channelId: nil, labels: nil)
             .sink { completion in
-                exp.fulfill()
+                switch completion {
+                case .finished:
+                    print("CalendarViewModelのデータ取得成功\(#function)")
+                    exp.fulfill()
+                case let .failure(error):
+                    XCTAssertNil(error)
+                }
+
             } receiveValue: { data in
                 XCTAssertNotNil(data)
+
             }
+            .store(in: &subscriptions)
+
         wait(for: [exp], timeout: 10)
-        //        let vm = CalendarViewModel(repository: repository)
-        //        vm.onAppear()
-        // onAppearの処理が終わるまで待機したい。
 
     }
 }
