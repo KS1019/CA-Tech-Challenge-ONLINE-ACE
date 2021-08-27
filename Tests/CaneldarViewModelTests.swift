981591d5b9640ce6f4836d4a23dd11fe19915ac9
techiro <techiromen0@gmail.com>
2021-08-27 20:32:15 +0900

    add: onAppearのテスト

--- Tests/CaneldarViewModelTests.swift
+++ Tests/CaneldarViewModelTests.swift
@@ -15,26 +15,11 @@ class CaneldarViewModelTests: XCTestCase {
     }
 
     func test_updateRepositoriesWhenOnAppear() {
-        //MARK: onAppearのテストができない
-        //        let vm = CalendarViewModel(repository: MockTimeTableRepository())
-        //        vm.onAppear()
-        //        XCTAssertTrue(!vm.timetables.isEmpty)
-        //テストの評価がonAppearより先に評価される
-        //onAppearが非同期のためテストできない。
-        //以下のテストはvm.onAppear()の処理
-        let repository = MockTimeTableRepository()
-        var subscriptions = Set<AnyCancellable>()
-
-        let exp = expectation(description: #function)
-        repository.fetchTimeTableData(firstAt: 1, lastAt: 2, channelId: nil, labels: nil)
-            .sink { completion in
-                switch completion {
-                case .finished:
-                    print("CalendarViewModelのデータ取得成功\(#function)")
-                    exp.fulfill()
-                case let .failure(error):
-                    XCTAssertNil(error)
-                }
+    
+        let vm = CalendarViewModel(repository: MockTimeTableRepository())
+        vm.onAppear()
+        XCTAssertTrue(!vm.timetables.isEmpty)
+    }
 
             } receiveValue: { data in
                 XCTAssertNotNil(data)
