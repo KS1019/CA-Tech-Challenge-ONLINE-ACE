07874d5e84ddd89c06e33b6681116e39677d8721
techiro <techiromen0@gmail.com>
2021-08-30 16:23:52 +0900

    add: テスト ReservedViewの変数

--- Tests/ReservedViewModelTest.swift
+++ Tests/ReservedViewModelTest.swift
@@ -15,4 +15,37 @@ class ReservedViewModelTest: XCTestCase {
         XCTAssertTrue(!vm.timetables.isEmpty)
     }
 
+    func test_updateRespsitoryesWhenDeleteReservation() {
+        let vm = ReservedViewModel(repository: MockTimeTableRepository())
+
+        // vm.onAppear()で空でないことを証明したのでローカルで要素を追加してもOK
+        vm.timetables.append(TimeTable(
+                                id: "mockTimetable",
+                                title: "mockTimetable",
+                                highlight: "mockTimetable",
+                                detailHighlight: "mockTimetable",
+                                startAt: 100,
+                                endAt: 200,
+                                channelId: "mockTimetable",
+                                labels: ["mockTimetable"],
+                                content: "mockTimetable"))
+
+        vm.deleteReservation(programId: "mockTimetable")
+
+    }
+
+    func test_failureDeleteReservationWhenAlertIsOn() {
+        let failureVm = ReservedViewModel(repository: MockTimeTableRepository(mode: .failure))
+        failureVm.deleteReservation(programId: "mockTimetable")
+        XCTAssertTrue(failureVm.isAlert)
+
+        let isAlert = failureVm.isAlert
+
+        let successVm = ReservedViewModel(repository: MockTimeTableRepository(mode: .success))
+        //失敗時のisAlertを代入して失敗時を再現
+        successVm.isAlert = isAlert
+        successVm.deleteReservation(programId: "mockTimetable")
+        XCTAssertFalse(successVm.isAlert)
+
+    }
 }
