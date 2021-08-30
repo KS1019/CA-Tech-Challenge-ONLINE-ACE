e744279fc4a3aad9f17d6c4c44c1996fd228c8bd
techiro <techiromen0@gmail.com>
2021-08-30 16:44:23 +0900

    add: Reservationのテスト

--- Tests/ReservedViewModelTest.swift
+++ Tests/ReservedViewModelTest.swift
@@ -12,37 +12,37 @@ class ReservedViewModelTest: XCTestCase {
     func test_updateRepositoriesWhenOnAppear() {
         let vm = ReservedViewModel(repository: MockTimeTableRepository())
         vm.onAppear()
-        XCTAssertTrue(!vm.timetables.isEmpty)
+        XCTAssertFalse(vm.timetables.isEmpty)
+        XCTAssertFalse(vm.labels.isEmpty)
+        XCTAssertFalse(vm.selectedGenreFilters.isEmpty)
     }
 
     func test_updateRespsitoryesWhenDeleteReservation() {
         let vm = ReservedViewModel(repository: MockTimeTableRepository())
 
-        // vm.onAppear()で空でないことを証明したのでローカルで要素を追加してもOK
-        vm.timetables.append(TimeTable(
-                                id: "mockTimetable",
-                                title: "mockTimetable",
-                                highlight: "mockTimetable",
-                                detailHighlight: "mockTimetable",
-                                startAt: 100,
-                                endAt: 200,
-                                channelId: "mockTimetable",
-                                labels: ["mockTimetable"],
-                                content: "mockTimetable"))
+        vm.onAppear()
 
         vm.deleteReservation(programId: "mockTimetable")
+        XCTAssertFalse(vm.isLoading)
+        XCTAssertFalse(vm.isLoading)
 
     }
 
     func test_failureDeleteReservationWhenAlertIsOn() {
+
         let failureVm = ReservedViewModel(repository: MockTimeTableRepository(mode: .failure))
+        failureVm.getReservaions()
+        XCTAssertTrue(failureVm.isAlert)
+        XCTAssertTrue(failureVm.isLoading)
         failureVm.deleteReservation(programId: "mockTimetable")
         XCTAssertTrue(failureVm.isAlert)
+        XCTAssertTrue(failureVm.labels.isEmpty)
+        XCTAssertTrue(failureVm.selectedGenreFilters.isEmpty)
 
         let isAlert = failureVm.isAlert
 
         let successVm = ReservedViewModel(repository: MockTimeTableRepository(mode: .success))
-        //失敗時のisAlertを代入して失敗時を再現
+        // 失敗時のisAlertを代入して失敗時を再現
         successVm.isAlert = isAlert
         successVm.deleteReservation(programId: "mockTimetable")
         XCTAssertFalse(successVm.isAlert)
